0d8d189272b9cb6fdb052be637404bd4f06faec9
dhamo97 <113776622+dhamo97@users.noreply.github.com>
2023-02-21 23:03:21 +0530

    Update B.java

--- Constructor/src/B.java
+++ Constructor/src/B.java
@@ -2,7 +2,7 @@
 public class B extends B1{
 	public B() {
 
-	    System.out.println("Default const...");
+	    System.out.println("Dhamooo");
 	  }
 	  public static void main(String[] args) {
     B a = new B();
